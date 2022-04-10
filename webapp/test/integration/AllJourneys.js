ffc5ca5d78902e11a26becfaa38d359e2c8b5d18
sridhar B <sbollabathula@answerthink.com>
2022-04-10 13:21:51 +0000

    Initial Commit

---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,18 @@
+// We cannot provide stable mock data out of the template.
+// If you introduce mock data, by adding .json files in your webapp/localService/mockdata folder you have to provide the following minimum data:
+// * At least 3 productsSet in the list
+// * All 3 productsSet have at least one toSuppliers
+
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Startup",
+	"./ListJourney",
+	"./NavigationJourney"
+], function (Opa5, Startup) {
+	"use strict";
+	Opa5.extendConfig({
+		arrangements: new Startup(),
+		viewNamespace: "r9masterdetail.view.",
+		autoWait: true
+	});
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+
+sap.ui.require([
+	"r9masterdetail/test/integration/AllJourneys"
+], function() {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
